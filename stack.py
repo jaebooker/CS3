fc2324dcb551fe5f7ee45fadde230074a3605436
jaebooker <jaesonbooker@gmail.com>
2019-06-17 00:01:44 -0700

    it seems the taste was not so sweet

--- stack.py
+++ stack.py
@@ -21,34 +21,37 @@ class LinkedStack(object):
 
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
-        if self.list.is_empty(self):
+        if self.list.is_empty():
             return True
         return False
 
     def length(self):
         """Return the number of items in this stack."""
-        return self.list.size()
+        return self.list.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(1) – Why? Because we don't have to traverse through the whole thing"""
-        self.list.prepend(self, item)
+        self.list.prepend(item)
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
-        if !self.list.is_empty(self):
+        if self.list.is_empty():
+            return None
+        else:
             return self.list.head.data
-        return None
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(1) – Why? No need to traverse list"""
-        if !self.list.is_empty(self):
-            self.list.next = self.list.head
-            return
-        raise ValueError('Stack is empty!')
+        if self.list.is_empty():
+            raise ValueError('Stack is empty!')
+        else:
+            deleted_item = self.list.head.data
+            self.list.delete(deleted_item)
+            return deleted_item
 
 
 # Implement ArrayStack below, then change the assignment at the bottom
