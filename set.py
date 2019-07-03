506f528157a0bb9267ff55a9d93e66eb0a35be5b
jaebooker <jaesonbooker@gmail.com>
2019-07-02 20:02:34 -0700

    more annotations and is subset

--- set.py
+++ set.py
@@ -29,3 +29,28 @@ class TheGodSet(object):
     Worst case running time: O(1) because it's just a basic check, no iteration needed"""
     def contains(self, key):
         return self.set.contains(key)
+
+
+    """Intersection() is where two sets collide, seeing where two sets have common interests... er, elements
+    Best and worst case running time: O(n) because all elements must be traversed in both sets"""
+    def intersection(self, set1, set2):
+        pass
+
+    """Union() joins two sets in unholy matromony, till deletion do they part, adding all unique elements
+    Best and worst case running time: O(n) because it must traverse all entries in both sets"""
+    def union(self, set1, set2):
+        pass
+
+    """Is_Subset() checks to see if there might be a small subset growing insight one of the sets,
+    with elements shared by the other set
+    Best case and worst case running time: O(m) where m is the subset that must be checked"""
+    def is_subset(self, set1, set2, subset):
+        for i in subset:
+            if self.set.contains(i) == False:
+                return False
+        return True
+
+    """Difference() is where two sets realize they may have a whole lot of different elements
+    Best and worst case running time: O(n) because it must traverse both sets completely"""
+    def difference(self, set1, set2):
+        pass
